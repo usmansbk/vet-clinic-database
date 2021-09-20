398551a4c29643a64968caf7c09fccecb77934c7
Usman Suleiman <usmansbk@gmail.com>
2021-09-20 12:06:04 +0100

    Query

--- queries.sql
+++ queries.sql
@@ -1,3 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon';
+
+SELECT * from animals WHERE date_of_birth BETWEEN 2016 and 2019;
