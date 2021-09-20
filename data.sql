cdd5a8c88dfa9c9e148e42813ceea02b7626c090
Usman Suleiman <usmansbk@gmail.com>
2021-09-20 11:58:08 +0100

    Fix date

--- data.sql
+++ data.sql
@@ -2,28 +2,28 @@
 
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
     'Agumon',
-    'Feb 3rd, 2020',
+    'Feb 3, 2020',
     10.23,
     true,
     0
 );
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
     'Gabumon',
-    'Nov 15th, 2018',
+    'Nov 15, 2018',
     8,
     true,
     2
 );
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
     'Pikachu',
-    'Jan 7th, 2021',
+    'Jan 7, 2021',
     15.04,
     false,
     1
 );
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
     'Devimon',
-    'May 12th, 2017',
+    'May 12, 2017',
     11,
     true,
     5
