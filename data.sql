0fbedaad7eda8b754a98e1090943f999fd9b3e2c
Usman Suleiman <usmansbk@gmail.com>
2021-09-20 13:28:58 +0100

    Add data

--- data.sql
+++ data.sql
@@ -28,3 +28,49 @@ INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) 
     true,
     5
 );
+
+
+/* Update and Query animals table */
+
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Charmander',
+    'Feb 8, 2020',
+    11,
+    false,
+    0
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Plantmon',
+    'Nov 15, 2022',
+    5.7,
+    true,
+    2
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Squirtle',
+    'Apr 2, 1993',
+    12.13,
+    false,
+    3
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Angemon',
+    'Jun 12, 2005',
+    45kg,
+    true,
+    1
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Boarmon',
+    'Jun 7, 2005',
+    20.4,
+    true,
+    7
+);
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (
+    'Blossom',
+    'Oct 13, 1998',
+    17,
+    true,
+    3
+);
