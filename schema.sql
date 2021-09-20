aa552fe16703f6618d4cef3eea462d7cbebd0530
Usman Suleiman <usmansbk@gmail.com>
2021-09-20 11:42:56 +0100

    Fix column name

--- schema.sql
+++ schema.sql
@@ -5,7 +5,7 @@ CREATE DATABASE vet_clinic;
 CREATE TABLE animals (
     id              INT GENERATED ALWAYS AS IDENTITY,
     name            VARCHAR(250),
-    data_of_birth   DATE,
+    date_of_birth   DATE,
     escape_attempts INT,
     neutered        BOOLEAN,
     weight_kg       FLOAT,
