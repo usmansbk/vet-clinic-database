478e2f9b63d015d080ac51100bfaf24b8ca7ce96
Usman Suleiman <usmansbk@gmail.com>
2021-09-20 12:27:29 +0100

    Add queries

--- queries.sql
+++ queries.sql
@@ -2,4 +2,16 @@
 
 SELECT * from animals WHERE name LIKE '%mon';
 
-SELECT * from animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 and 2019;
+SELECT name from animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 2016 and 2019;
+
+SELECT name from animals WHERE neutered IS TRUE AND escape_attempts < 3;
+
+SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
+
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+
+SELECT * from animals WHERE neutered IS TRUE;
+
+SELECT * from animals WHERE name NOT LIKE 'Gabumon';
+
+SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
