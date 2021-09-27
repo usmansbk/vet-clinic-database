0a0ee0ddec3e2216d6af0d92edb908ff7d84004f
Tolibjon <tolibjohn@gmail.com>
2021-09-27 17:21:46 +0500

    Create treatments, invoice_items and join table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -23,3 +23,27 @@ CREATE TABLE invoices (
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE treatments(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  type VARCHAR(50),
+  name VARCHAR(50)
+);
+
+CREATE TABLE treatment(
+  medical_history_id INT,
+  treatment_id INT,
+  FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
+  FOREIGN KEY(treatment_id) REFERENCES treatments(id)
+);
+
+CREATE TABLE invoice_items(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price decimal,
+  invoice_id INT,
+  treatment_id INT,
+  FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+  FOREIGN KEY(treatment_id) REFERENCES treatments(id)
+);
