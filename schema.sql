7db0041875048d3f4ea9bc8fc5d00196482a0290
Usman Suleiman <usmansbk@gmail.com>
2021-09-27 12:08:15 +0100

    Apply changes

--- schema.sql
+++ schema.sql
@@ -68,12 +68,6 @@ CREATE TABLE visits (
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
--- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
-INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
-
--- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
-insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
-
 -- Optimize visits table by creating an Index using the animals_id column
 CREATE INDEX animals_id_asc ON visits (animals_id ASC);
 
@@ -81,4 +75,4 @@ CREATE INDEX animals_id_asc ON visits (animals_id ASC);
 CREATE INDEX vets_id_asc ON visits (vets_id ASC);
 
 -- -- Optimize owners table by creating an Index using the email column
-CREATE INDEX owners_email_asc ON owners (email ASC);
+CREATE INDEX owners_email_asc ON owners ((lower(email)));
